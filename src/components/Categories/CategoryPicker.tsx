37be25a8409e8d6fd63a6b3a53c57d635baacd09
Bogdan Boiculese <bogdan.boiculese@gmail.com>
2022-09-10 10:29:58 +0100

    better category selection

--- src/components/Categories/CategoryPicker.tsx
+++ src/components/Categories/CategoryPicker.tsx
@@ -1,9 +1,10 @@
-import SelectSearch, { fuzzySearch, SelectedOptionValue, SelectSearchOption } from 'react-select-search';
 import shallow from 'zustand/shallow';
 import { trpc } from '../../utils/trpc';
 import { useAppStore } from '../../store/appStore';
 import { useHotkeys } from 'react-hotkeys-hook';
-import { useEffect, useMemo, useRef } from 'react';
+import React, { useCallback, useEffect, useMemo, useRef, useState } from 'react';
+import classNames from 'classnames';
+import { Category } from '@prisma/client';
 
 export function CategoryPicker(): JSX.Element | null {
   const [setCurrentCategoryId, currentCategoryId, showAddCategory, toggleAddCategory] = useAppStore(
@@ -11,44 +12,113 @@ export function CategoryPicker(): JSX.Element | null {
     shallow
   );
 
-  const ref = useRef<any>(null);
-
   const categories = trpc.useQuery(['categories.all'], {
     cacheTime: Infinity,
   });
 
-  useEffect(() => {
-    if (categories.data && categories.data[0]) {
-      setCurrentCategoryId(categories.data[0].id);
-    }
-  }, [categories.data, setCurrentCategoryId]);
+  const [searchText, setSearchText] = useState('');
+  const [showList, setShowList] = useState(false);
+  const [currentCategoryIndex, setCategoryIndex] = useState(0);
+  const [applyFilter, setApplyFilter] = useState(false);
 
-  const options = useMemo(
-    () =>
-      categories.data?.map(c => ({
-        name: c.title,
-        value: c.id,
-      })) || [],
-    [categories.data]
-  );
+  const filteredCategories = useMemo(() => {
+    const result = applyFilter
+      ? categories.data?.filter(c => c.title.toLowerCase().includes(searchText.toLowerCase()))
+      : categories.data;
+
+    if (result?.length && result.length - 1 < currentCategoryIndex) {
+      setCategoryIndex(0);
+    }
+
+    return result;
+  }, [applyFilter, categories.data, currentCategoryIndex, searchText]);
+
+  const ref = useRef<HTMLInputElement>(null);
+
+  useEffect(() => {
+    if (!currentCategoryId && filteredCategories && filteredCategories[0]) {
+      setCurrentCategoryId(filteredCategories[0].id);
+      setSearchText(filteredCategories[0].title);
+    }
+  }, [currentCategoryId, filteredCategories, setCurrentCategoryId]);
 
   useHotkeys('/', event => {
     event.preventDefault();
-    ref.current?.querySelector('input')?.focus();
+    ref.current?.focus();
+    ref.current?.select();
   });
+
+  const handleSelected = useCallback(
+    (category: Category) => {
+      const index = Math.max(0, categories.data?.findIndex(c => c.id === category.id) || 0);
+      setCategoryIndex(index);
+      setCurrentCategoryId(category.id);
+      setSearchText(category.title);
+      setApplyFilter(false);
+    },
+    [categories.data, setCurrentCategoryId]
+  );
+
+  const handleKeyDown = useCallback(
+    (e: React.KeyboardEvent<HTMLInputElement>) => {
+      if (e.key === 'Escape') {
+        const category = categories.data?.find(c => c.id === currentCategoryId);
+
+        if (category) {
+          setSearchText(category.title);
+        }
+
+        setApplyFilter(false);
+        e.currentTarget.blur();
+      } else if (!filteredCategories) {
+        return;
+      } else if (e.key === 'ArrowUp') {
+        e.preventDefault();
+        setCategoryIndex(Math.max(currentCategoryIndex - 1, 0));
+      } else if (e.key === 'ArrowDown') {
+        e.preventDefault();
+        setCategoryIndex(Math.min(currentCategoryIndex + 1, filteredCategories.length - 1));
+      } else if (e.key === 'Enter') {
+        const category = filteredCategories[currentCategoryIndex];
+        if (category) {
+          handleSelected(category);
+        }
+        e.currentTarget.blur();
+      } else if (e.key.match(/^[a-z]$/)) {
+        // when a key is pressed, apply filter
+        setApplyFilter(true);
+      }
+    },
+    [filteredCategories, categories.data, currentCategoryId, currentCategoryIndex, handleSelected]
+  );
 
   return (
     <div className="flex w-full justify-between mb-2">
-      <SelectSearch
+      <input
+        className="bg-transparent p-1"
         ref={ref}
-        value={options.find(o => o.value === currentCategoryId)?.value}
-        filterOptions={fuzzySearch}
-        search={true}
-        autoComplete="on"
-        options={options}
-        placeholder="Search categories"
-        onChange={(e: any) => setCurrentCategoryId(e)}
+        onFocus={() => setShowList(true)}
+        onBlur={() => setShowList(false)}
+        value={searchText}
+        onChange={e => setSearchText(e.currentTarget.value)}
+        onKeyDown={handleKeyDown}
+        type="text"
       />
+      {showList && (
+        <div className="absolute top-14 bg-primary border-4 min-w-[150px]">
+          {filteredCategories?.map((c, i) => (
+            <div
+              key={c.id}
+              onMouseDown={() => handleSelected(c)}
+              className={classNames('p-2 cursor-pointer', {
+                'font-bold text-sky-400': i === currentCategoryIndex,
+              })}
+            >
+              {c.title}
+            </div>
+          ))}
+        </div>
+      )}
       {!categories.isLoading && categories.data && (
         <div onClick={toggleAddCategory}>{showAddCategory ? '(-)' : '(+)'}</div>
       )}
